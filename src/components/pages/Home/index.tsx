00c6659a66aea31b0619d3472059279e9274d348
ninjaguyyyy <ninjaguyyyy@gmail.com>
2023-02-15 08:04:14 +0700

    fix: fix

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,3 +1,16 @@
+import './index.scss';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="home">
+      <div className="home__side">
+        <div className="home__side__avatar">avatar</div>
+        <div className="home__side__avatar">name</div>
+        <div className="home__side__avatar">title</div>
+        <div className="home__side__avatar">description</div>
+        <div className="home__side__avatar"></div>
+      </div>
+      <div className="home__main-content">content</div>
+    </div>
+  );
 }
